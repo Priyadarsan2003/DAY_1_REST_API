c0e140b1861eea38a67d06e7a0c5cd7ebbcd00ff
Priyadarsan2003 <108514634+Priyadarsan2003@users.noreply.github.com>
2023-08-28 16:22:30 +0530

    Delete DAY_04 directory
    Signed-off-by: Priyadarsan2003 <108514634+Priyadarsan2003@users.noreply.github.com>

--- DAY_04/src/main/java/com/example/demo/repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.StudentEntity;
-
-public interface StudentRepo extends JpaRepository<StudentEntity, Integer>{
-	StudentEntity getById(int id);
-}
